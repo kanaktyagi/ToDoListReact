463a072413e354cb5e4c5180d0ca8aea503a10e2
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-27 12:28:58 +0530

    Added the method to add itmes in the array and to display it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const[inputList, setInputList] =useState("");
+  const[Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  }
+  const listOfItems = () => {
+    setItems((oldItems)=> {
+      return [...oldItems, inputList]
+    })
+  }
   return (
     <div className="main_div">
       <div className="center_div">
       <br/>
         <h1>ToDO List </h1>
       <br/>
-        <input type="text" placeholder="Add a item" />
-        <button > + </button>  
+        <input type="text" placeholder="Add a item" onChange={itemEvent} />
+        <button  onClick={listOfItems}> + </button>  
         <ol>
-          <li>buy apple</li>
+         {
+          Items.map(itemVal => {
+            return <li>{itemVal}</li>
+          })
+         }
         </ol>
       </div>
     </div>
